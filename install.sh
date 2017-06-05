28eb37fecc3c0ecf48b4b76acb20b1a4e3f45bee
Ashwin Madavan <ashwin.madavan@gmail.com>
2017-06-05 11:50:33 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,35 +12,37 @@ brew upgrade
 # Install Tools
 brew install cask    
 brew tap homebrew/science    
-brew install r
 brew cask install java
-brew install tomcat
-brew install android-sdk
 brew install scala --with-docs
-brew install node --with-docs
-brew install boost
-brew install cmake
-brew install mysql
-brew install maven
-brew install git-lfs
 brew install bash-completion
 
+# brew install git-lfs
+# brew install r
+# brew install node --with-docs
+# brew install tomcat
+# brew install android-sdk
+# brew install boost
+# brew install cmake
+# brew install mysql
+# brew install maven
+
 # Install Applications
-brew tap caskroom/versions
-brew cask install insync
-open /Applications/Insync.app
-brew cask install insomniax
-open /Applications/InsomniaX.app
-brew cask install beardedspice
-open /Applications/BeardedSpice.app
 brew cask install intellij-idea-ce
 open /Applications/IntelliJ\ IDEA\ CE.app
-brew cask install steam
-open /Applications/Steam.app
 brew cask install google-chrome
 open /Applications/Google\ Chrome.app
-brew cask install amethyst
-open /Applications/Amethyst.app
+brew cask install spectacle
+open /Applications/Spectacle.app
+
+# brew tap caskroom/versions
+# brew cask install insync
+# open /Applications/Insync.app
+# brew cask install insomniax
+# open /Applications/InsomniaX.app
+# brew cask install beardedspice
+# open /Applications/BeardedSpice.app
+# brew cask install steam
+# open /Applications/Steam.app
 
 # Setup Workspace
 git clone https://github.com/ashwin153/tools.git ~/Documents/workspace/tools
