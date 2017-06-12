0c3d7f6e3dc21e5ef8fa6df23a2332d362ff0a63
Ashwin Madavan <ashwin.madavan@gmail.com>
2017-06-12 15:22:46 -0700

    Create install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-cp .vimrc ~/.vimrc
-cp .bash_profile ~/.bash_profile
-
 # Install Command Line Tools
 xcode-select --install
 
@@ -10,47 +7,24 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 sudo chown -R $(whoami) /usr/local
 brew update
 brew doctor
-brew upgrade
+brew upgrade 
 
 # Install Tools
 brew install cask    
-brew tap homebrew/science    
+brew tap homebrew/science   
 brew cask install java
-brew install scala --with-docs
+brew install scala
 brew install bash-completion
-
-# brew install git-lfs
-# brew install r
-# brew install node --with-docs
-# brew install tomcat
-# brew install android-sdk
-# brew install boost
-# brew install cmake
-# brew install mysql
-# brew install maven
+brew install git-lfs
 
 # Install Applications
 brew cask install intellij-idea-ce
-open /Applications/IntelliJ\ IDEA\ CE.app
 brew cask install google-chrome
-open /Applications/Google\ Chrome.app
 brew cask install spectacle
-open /Applications/Spectacle.app
+brew cask install steam
 
-# brew tap caskroom/versions
-# brew cask install insync
-# open /Applications/Insync.app
-# brew cask install insomniax
-# open /Applications/InsomniaX.app
-# brew cask install beardedspice
-# open /Applications/BeardedSpice.app
-# brew cask install steam
-# open /Applications/Steam.app
-
-# Setup Workspace
-git clone https://github.com/ashwin153/tools.git ~/Documents/workspace/tools
-cp ~/Documents/workspace/tools/vimrc ~/.vimrc
-cp ~/Documents/workspace/tools/bash_profile ~/.bash_profile
-ssh-keygen -t rsa -b 4096 -C "ashwin.madavan@gmail.com"
+# Setup Workspace`
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cp .vimrc ~/.vimrc
+cp .bash_profile ~/.bash_profile
 vim +PluginInstall +qall
