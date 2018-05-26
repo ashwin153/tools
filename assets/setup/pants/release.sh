ca647d5d981b1962556161939b7db9581db8af3a
Ashwin Madavan <ashwin.madavan@gmail.com>
2018-05-26 07:34:06 +0100

    [setup] Updated pants project.

--- assets/setup/pants/release.sh
+++ assets/setup/pants/release.sh
@@ -1,33 +1,35 @@
 #!/bin/bash
+####################################################################################################
+#                               Declare Build Targets and Artifacts                                #
+#                        https://github.com/ashwin153/caustic/wiki/Release                         #
+####################################################################################################
 declare -a targets=()
 
 declare -a artifacts=()
 
-# Verify Branch is Clean.
+####################################################################################################
+#                                Publish Artifacts to Maven Central                                #
+#                                          DO NOT MODIFY                                           #
+####################################################################################################
 branch=$(git symbolic-ref --short HEAD)
 if [ -n "$(git status --porcelain)" ]; then 
   echo -e "Current branch \033[0;33m$branch\033[0m has uncommitted changes."
   exit 1
+else
+  read -p "Artifact version (defaults to incrementing patch version): " version
+  read -r -p "$(echo -e -n "Confirm release of \033[0;33m$branch\033[0;0m? [y|N] ")" response
 fi
 
-# Release Guidelines: https://github.com/ashwin153/caustic/wiki/Release
-read -p "Artifact version (defaults to incrementing patch version): " version
-read -r -p "$(echo -e -n "Confirm release of \033[0;33m$branch\033[0;0m? [y|N] ")" response
-
-# Publish Build Artifacts.
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]] ; then
   if [ -z "$version" ] ; then
-    # Increment Patch Version.
     publish="./pants publish.jar --publish-jar-no-dryrun ${targets[@]}"
   else
-    # Override Artifact Version.
     overrides=("${artifacts[@]/#/--publish-jar-override=com.madavan#}")
     overrides=("${overrides[@]/%/=$version}")
     publish="./pants publish.jar --publish-jar-no-dryrun ${overrides[@]} ${targets[@]}"
   fi
 
-  if eval $publish ; then 
-    # Promote to Maven Central.
+  if eval "./pants test ::" && eval $publish ; then
     /usr/bin/open -a "/Applications/Google Chrome.app" \
       'http://www.pantsbuild.org/release_jvm.html#promoting-to-maven-central'
     /usr/bin/open -a "/Applications/Google Chrome.app" \
